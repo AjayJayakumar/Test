b2c57fd9a83928f2c29464f9a8657e139f1342c7
Ajay <ajay.jayakumar@livares.com>
2022-09-06 21:21:51 +0530

    Detail API
    Get Text Snippet Details

--- django_app/text_snippets/urls.py
+++ django_app/text_snippets/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from .views import *
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+# router.register('text-snippet/get',views.GetTextSnippetViewset, basename='provider-suspence-account')
+
 
 urlpatterns = [
   path("new-user-login",NewUserAPIView.as_view()),
   path("text-snippet/add",AddingNewTextSnippet.as_view()),
+  path("text-snippet/get/<int:pk>/",GetTextSnippet.as_view()),
 ]
+
+urlpatterns += router.urls
--- django_app/text_snippets/views.py
+++ django_app/text_snippets/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from .models import TextSnippets
 from .serializers import NewUserSerializer, TextSnippetSerializer
 from django.contrib.auth.models import User
-from rest_framework import generics,status
+from rest_framework import generics,status,viewsets
 
 
 
@@ -41,4 +41,32 @@ class AddingNewTextSnippet(APIView):
         return Response(response_data, status=resp_status)
        
        
-       
+# Get Text Snippet Details           
+class GetTextSnippet(APIView):
+    
+    def get(self,request,pk):
+        response_data = {}
+        data = {}
+        data_to_view = TextSnippets.objects.filter(id=pk)
+        print(data_to_view)
+        if data_to_view:
+            retrieved_data = {
+            'title': data_to_view.values_list('title'), 
+            'created_time': data_to_view.values_list('created_time'),
+            'created_user': data_to_view.values_list('created_by_id')
+            }
+
+            data = retrieved_data
+            if data:
+                response_data['status_code'] = '200'
+                response_data['status'] = True
+                response_data['message'] = 'Text Snippet Details fetched. '
+                response_data['data'] = data
+                resp_status = status.HTTP_200_OK
+            else:
+                response_data['status_code'] = '400'
+                response_data['status'] = False
+                response_data['message'] = 'Text Snippet Details not fetched.'
+                resp_status = status.HTTP_400_BAD_REQUEST
+
+        return Response(response_data, status=resp_status)
